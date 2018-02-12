1e01f89a7dedfef5e4d48e1e8aa35194dac64708
Moises Goncalves <moises@lab15.io>
2018-02-12 14:40:23 +0000

    Merged PR 198: Added axios http library for external http requests!
    Added axios http library for external http requests!

--- ps-backend/app/controllers/authentication-controller.js
+++ ps-backend/app/controllers/authentication-controller.js
@@ -1,9 +1,22 @@
+//http library
+var axios = require('axios');
 var sha1 = require('sha1');
 
 module.exports = function (app) {
     app.post('/authentication/signin', function (req, res) {
         
-		//query blockchain for user credentials
+		//query blockchain for user credentials http using axios
+		/*
+		axios.get(reqEnd)
+            .then(resp => {
+                console.log(resp.data);
+                response.send(resp.data);
+            })
+            .catch(error => {
+                console.log(error);
+                response.send({ success: false, data: error });
+            });
+		*/
 		
 		var user = { 'email': 'admin@cv.nl', 'password': 'admin' };
 		if (user.password == sha1(req.body.password)) {
