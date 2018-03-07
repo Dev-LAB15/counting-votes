f2c7d313d9c88394c54766906158d6c41b633785
Jefferson Pereira da Silva <jeff@lab15.io>
2018-03-07 16:54:55 +0000

    Merged PR 243: Merge resportScreen to master

---
+++ ps-ui/js/overview.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function () {
+    var vm = new Vue({
+        i18n,
+        el: '#app',
+        data: {
+            chairman: this.window.localStorage.chairman,
+            tellers: getTellers()
+        },
+        methods: {
+        }
+
+    });
+});
