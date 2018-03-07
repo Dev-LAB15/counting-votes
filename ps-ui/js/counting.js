5ca05fb1f6fe35c2a929684279a8f939fffd9ad8
Moises Goncalves <moises@lab15.io>
2018-03-07 08:48:48 +0000

    Merged PR 234: review the merge of counting.html screen.
    review the merge of counting.html screen.

--- ps-ui/js/counting.js
+++ ps-ui/js/counting.js
@@ -14,10 +14,10 @@ window.addEventListener('load', function () {
                 var vote = { option: voteId };
                 axios.post(apiEndpoint + '/counting/vote', vote, axiosHeaders)
                     .then(resp => {
-                  
+
                     }
                     ).catch(error => {
-                  
+
                     }
                     );
 
@@ -31,24 +31,6 @@ window.addEventListener('load', function () {
         var body = document.body,
             dropArea = document.getElementById('drop-area'),
             droppableArr = [], dropAreaTimeout;
-
-        // initialize droppables
-        [].slice.call(document.querySelectorAll('#drop-area .drop-area__item')).forEach(function (el) {
-            droppableArr.push(new Droppable(el, {
-                onDrop: function (instance, draggableEl) {
-                    // show checkmark inside the droppabe element
-                    classie.add(instance.el, 'drop-feedback');
-                    clearTimeout(instance.checkmarkTimeout);
-                    instance.checkmarkTimeout = setTimeout(function () {
-                        classie.remove(instance.el, 'drop-feedback');
-
-                        draggableEl.style.left = '50%';
-                        draggableEl.style.top = '50%';
-                    }, 600);
-                    // ...
-                }
-            }));
-        });
 
         // initialize draggable(s)
         [].slice.call(document.querySelectorAll('#grid .grid__item')).forEach(function (el) {
@@ -80,6 +62,24 @@ window.addEventListener('load', function () {
                     }
                 }
             });
+        });
+
+        // initialize droppables
+        [].slice.call(document.querySelectorAll('#drop-area .drop-area__item')).forEach(function (el) {
+            droppableArr.push(new Droppable(el, {
+                onDrop: function (instance, draggableEl) {
+                    // show checkmark inside the droppabe element
+                    classie.add(instance.el, 'drop-feedback');
+                    clearTimeout(instance.checkmarkTimeout);
+                    instance.checkmarkTimeout = setTimeout(function () {
+                        classie.remove(instance.el, 'drop-feedback');
+                        draggableEl.style.left = '50%';
+                        draggableEl.style.top = '50%';
+                        vm.registerVote(instance.el.id);
+                    }, 600);
+                    // ...
+                }
+            }));
         });
     })();
 });
