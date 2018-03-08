34e654e65f0d597a02eaf59d107fea332282df0f
Moises Goncalves <moises@lab15.io>
2018-03-08 20:53:07 +0000

    Merged PR 254: fix remote ui environment
    fix remote ui environment

--- ps-backend/app/controllers/transaction-controller.js
+++ ps-backend/app/controllers/transaction-controller.js
@@ -1,3 +1,7 @@
+var Web3 = require('web3');
+var config = require('../../config.json');
+var web3 = new Web3(new Web3.providers.WebsocketProvider(config.blockchain.wsProvider));
+
 var psContract = require('../contracts/polling.station.contract');
 var mnContract = require('../contracts/municipality.contract');
 
@@ -22,6 +26,32 @@ module.exports = function (app) {
             //reverses the array
             voterEvents.reverse();
             console.log(voterEvents);
+            res.json(voterEvents);
+        });
+    });
+
+    app.get('/transaction/log', function (req, res) {
+
+        mnContract.getPastEvents(function (err, events) {
+            var sendBack = true;
+            //the voter events can't exceed a maximum value of 10
+            var voterEvents = [];
+            for (let i = 0; i < events.length; i++) {
+                let event = events[i];
+                event.blockNumber;
+                switch (event.event) {
+                    case "VoterCleared":
+                        voterEvents.push({ transactionHash: event.transactionHash, blockNumber: event.blockNumber, error: false });
+                        break;
+                    case "VoterAlreadyRecorded":
+                        voterEvents.push({ transactionHash: event.transactionHash, blockNumber: event.blockNumber, error: true });
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
+
             res.json(voterEvents);
         });
     });
