4f54ad425c1b79f8aabe5412310725c2928d30bb
Moises Goncalves <moises@lab15.io>
2018-03-08 07:57:51 +0000

    Merged PR 242: refactor the qr-code reader to data-matrix code reader.
    refactor the qr-code reader to data-matrix code reader.

--- ps-ui/js/scan.js
+++ ps-ui/js/scan.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function () {
 
 
+
     let tellers = getTellers();
 
 
@@ -16,35 +17,6 @@ window.addEventListener('load', function () {
             }
         },
         methods: {
-            /**
-             * Decodes a QR Code content from the camera.
-             * @param {qrcode decoded content} content 
-             */
-            onDecode(content) {
-                $('#confirm-qrcode-modal').modal()
-                    .one('click', '#button-yes', function (e) {
-
-                        axios.post(apiEndpoint + '/scan/qrcode', vm.model, axiosHeaders)
-                            .then(resp => {
-                                vm.$toasted.show(this.$t('message.qrCodeScannedSuccessfully'), {
-                                    theme: "outline",
-                                    position: "bottom-center",
-                                    duration: 3000
-                                });
-                            })
-                            .catch(error => {
-                                console.log(error.response);
-                                //TODO fix 403 error accordingly.
-                            }
-                            );
-                    });
-
-            },
-
-            onLocate(points) {
-
-            },
-
             manualInput: function () {
                 axios.post(apiEndpoint + '/scan/qrcode', vm.model, axiosHeaders)
                     .then(resp => {
--- ps-ui/server.js
+++ ps-ui/server.js
@@ -25,10 +25,11 @@ http.createServer(function (req, res) {
     '.pdf': 'application/pdf',
     '.doc': 'application/msword',
     '.eot': 'appliaction/vnd.ms-fontobject',
-    '.ttf': 'aplication/font-sfnt'
+    '.ttf': 'aplication/font-sfnt',
+    '.wasm': 'binary/octet-stream'
   };
   fs.exists(pathname, function (exist) {
-    if(!exist) {
+    if (!exist) {
       // if the file is not found, return 404
       res.statusCode = 404;
       res.end(`File ${pathname} not found!`);
@@ -39,15 +40,15 @@ http.createServer(function (req, res) {
       pathname += '/index.html';
     }
     // read file from file system
-    fs.readFile(pathname, function(err, data){
-      if(err){
+    fs.readFile(pathname, function (err, data) {
+      if (err) {
         res.statusCode = 500;
         res.end(`Error getting the file: ${err}.`);
       } else {
         // based on the URL path, extract the file extention. e.g. .js, .doc, ...
         const ext = path.parse(pathname).ext;
         // if the file is found, set Content-type and send data
-        res.setHeader('Content-type', mimeType[ext] || 'text/plain' );
+        res.setHeader('Content-type', mimeType[ext] || 'text/plain');
         res.end(data);
       }
     });
