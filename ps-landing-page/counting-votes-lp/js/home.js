22dcc3e1e5235b630d2436daa73b472beca2a021
Jefferson Pereira da Silva <jeff@lab15.io>
2018-03-16 07:22:47 +0000

    Merged PR 279: Auto stash before merge of "master" and "origin/master"
    Auto stash before merge of "master" and "origin/master"
    
    signout screen

--- ps-landing-page/counting-votes-lp/js/home.js
+++ ps-landing-page/counting-votes-lp/js/home.js
@@ -27,4 +27,80 @@ $(function(){
 	    }
 	  ]
 	});
+
+	function countingVotes(){
+		$('.counter').each(function(index, el) {
+			var votes = $(el).data('votes');
+			var speed = $(el).data('speed');
+
+			$(el).text(votes);
+
+			var interval = setInterval(function() {
+				votes = votes + 1;
+
+				$(el).text(votes);
+				
+			}, speed);
+		});
+	}
+
+	function progressBar(){
+		$('.progress-bar').each(function(index, el) {
+			var amount = 1;
+			var speed = $(el).data('speed');
+
+			$(el).css('width', amount + '%');
+			
+			var interval = setInterval(function() {
+				amount = amount + 1;
+
+				$(el).css('width', amount + '%');
+			}, speed);
+		});
+	}
+
+	function countingVotes(){
+		$('.counter').each(function(index, el) {
+			var votes = $(el).data('votes');
+			var speed = $(el).data('speed');
+
+			$(el).text(votes);
+
+			var interval = setInterval(function() {
+				votes = votes + 1;
+
+				$(el).text(votes);
+				
+			}, speed);
+		});
+	}
+
+	function progressBar(){
+		$('.progress-bar').each(function(index, el) {
+			var amount = 1;
+			var speed = $(el).data('speed');
+
+			$(el).css('width', amount + '%');
+			
+			var interval = setInterval(function() {
+				amount = amount + 1;
+
+				$(el).css('width', amount + '%');
+			}, speed);
+		});
+	}
+
+	countingVotes();
+	progressBar();
+
+	Circles.create({
+		id:           'avg-rate',
+		radius:       100,
+		value:        43,
+		maxValue:     100,
+		width:        15,
+		text:         function(value){return value + '%';},
+		wrpClass:     'circles-wrp',
+		textClass:    'circles-text'
+	})
 });
